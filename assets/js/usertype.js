406949d5216f7022104d6c5992b08ae442f8843c
Jonathan Sutcliffe <jonathan.sutcliffe@gov.scot>
2019-07-01 13:02:32 +0100

    MGS-5309 Workaround: calls to userType endpoint intermittently failing on SCOTS - provide fallback

--- assets/js/usertype.js
+++ assets/js/usertype.js
@@ -1,9 +1,12 @@
 (function() {
+    // default to unspecified
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer[0].userType = 'unspecified';
+
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if(xhr.readyState === 4 && xhr.status === 200) {
             var response = JSON.parse(xhr.responseText);
-            window.dataLayer = window.dataLayer || [];
             window.dataLayer[0].userType = response.userType;
         }
     };
